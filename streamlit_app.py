a3998ff23face2ab3db03bc3250084c858394340
Rashika17 <141156719+Rashika17@users.noreply.github.com>
2023-08-01 20:10:37 +0530

    Update streamlit_app.py
    updated header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My parents New Healthy diner')
+streamlit.title('My Mom's New Healthy diner')
 streamlit.header('BREAKFAST Favourites')
 streamlit.text('🥣 Omega3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
